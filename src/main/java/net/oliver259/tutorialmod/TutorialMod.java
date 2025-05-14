fc125c39f796a8a2fa6120da82598590d081c2a4
Oliver259 <115050339+Oliver259@users.noreply.github.com>
2025-05-14 17:06:19 +1000

    Added bismuth items and bismuth blocks creative mode tabs

--- src/main/java/net/oliver259/tutorialmod/TutorialMod.java
+++ src/main/java/net/oliver259/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.oliver259.tutorialmod;
 
 import net.minecraft.world.item.CreativeModeTabs;
 import net.oliver259.tutorialmod.block.ModBlocks;
+import net.oliver259.tutorialmod.item.ModCreativeModeTabs;
 import net.oliver259.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 
@@ -36,6 +37,8 @@ public class TutorialMod {
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
+
+        ModCreativeModeTabs.register(modEventBus);
 
         ModItems.register(modEventBus);
         ModBlocks.register(modEventBus);
