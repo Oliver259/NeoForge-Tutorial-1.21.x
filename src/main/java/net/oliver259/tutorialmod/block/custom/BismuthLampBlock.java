9bc52df0aad53c44311466bfd989d19d06f0d463
Oliver259 <115050339+Oliver259@users.noreply.github.com>
2025-06-16 13:03:32 +1000

    Made Bismuth Lamp work with redstone signals

--- src/main/java/net/oliver259/tutorialmod/block/custom/BismuthLampBlock.java
+++ src/main/java/net/oliver259/tutorialmod/block/custom/BismuthLampBlock.java
@@ -1,8 +1,11 @@
 package net.oliver259.tutorialmod.block.custom;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
@@ -10,12 +13,45 @@ import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.phys.BlockHitResult;
 
+import javax.annotation.Nullable;
+
 public class BismuthLampBlock extends Block {
     public static final BooleanProperty CLICKED = BooleanProperty.create("clicked");
 
     public BismuthLampBlock(Properties properties) {
         super(properties);
         this.registerDefaultState(this.defaultBlockState().setValue(CLICKED, false));
+    }
+
+    @Override
+    @Nullable
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return this.defaultBlockState().setValue(CLICKED, context.getLevel().hasNeighborSignal(context.getClickedPos()));
+    }
+
+    @Override
+    protected void neighborChanged(BlockState state, Level level, BlockPos pos, Block neighborBlock, BlockPos neighborPos, boolean movedByPiston) {
+        if (!level.isClientSide) {
+            boolean powered = level.hasNeighborSignal(pos);
+            boolean lit = state.getValue(CLICKED);
+
+            if (powered != lit) {
+                if (lit) {
+                    level.scheduleTick(pos, this, 4);
+                } else {
+                    level.setBlock(pos, state.setValue(CLICKED, true), 2);
+                }
+            }
+        }
+
+    }
+
+    @Override
+    protected void tick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
+        if (state.getValue(CLICKED) && !level.hasNeighborSignal(pos)) {
+            level.setBlock(pos, state.setValue(CLICKED, false), 2);
+        }
+
     }
 
     @Override
